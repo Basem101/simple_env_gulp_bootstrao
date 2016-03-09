5bcdfcea37484340d49a9ab7a2bcecfd09f9f841
Basem Morkos <Basem@Basem.local>
2016-03-09 10:43:39 -0500

    simple env with gulp and bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+// including plugins
+    var gulp      = require('gulp');
+        less      = require("gulp-less"),
+        minifycss = require('gulp-minify-css'),
+        rename    = require('gulp-rename'),
+        plumber   = require('gulp-plumber'),
+
+    // Task - Compiles Unchained Less Files
+    gulp.task('compile-bp-less', function () {
+      gulp.src('static/less/bootstrap/bootstrap.less') // path to your file
+      .pipe(plumber())
+      .pipe(less())
+      .pipe(gulp.dest('static/css'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(minifycss())
+      .pipe(gulp.dest('static/css'));
+    });
+
+    // Task - Watches Unchained + bp Less Files
+    gulp.task('watch-bp-less', function () {
+       gulp.watch('static/less/bootstrap/**/*.less', ['compile-bp-less']);
+       gulp.watch('static/less/bootstrap/*.less', ['compile-bp-less']);
+    });
+
+    // Task - 'Gulp' Command in terminal
+    gulp.task('default', ['compile-bp-less', 'watch-bp-less'],
+        function () {
+    });
